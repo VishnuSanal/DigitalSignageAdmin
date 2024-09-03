20471ccfc19ed97998fb4dabd561f2b950d899bc
VishnuSanal <t.v.s10123@gmail.com>
2024-09-03 16:38:57 +0530

    :)

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -21,21 +21,40 @@ fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
 
-        /*
-        GlobalScope.launch {
-            firebaseDatabaseAPI.setAnnouncements(
-                listOf(
-                    Announcement("College buses depart at 14:30 today"),
-                    Announcement("Classes are suspended from 15:00"),
-                    Announcement("Library will be open till 21:30 until exams are over"),
-                    Announcement("Knimbus Digital Library registration ends tomorrow"),
-                    Announcement("S2 exam registration deadline extended"),
-                    Announcement("Digital Signage system back up again!"),
-                    Announcement("Tomorrow is a holiday!"),
-                )
-            )
-        }
-        */
+//        GlobalScope.launch {
+//            firebaseDatabaseAPI.setAnnouncements(
+//                listOf(
+//                    Announcement(
+//                        "College buses depart at 14:30 today",
+//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+//                    ),
+//                    Announcement(
+//                        "Classes are suspended from 15:00",
+//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+//                    ),
+//                    Announcement(
+//                        "Library will be open till 21:30 until exams are over",
+//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+//                    ),
+//                    Announcement(
+//                        "Knimbus Digital Library registration ends tomorrow",
+//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+//                    ),
+//                    Announcement(
+//                        "S2 exam registration deadline extended",
+//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+//                    ),
+//                    Announcement(
+//                        "Digital Signage system back up again!",
+//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+//                    ),
+//                    Announcement(
+//                        "Tomorrow is a holiday!",
+//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+//                    ),
+//                )
+//            )
+//        }
 
     }
 }
