b205aa5261c3803f01d967d21317dbc5bff9782f
Vishnu Sanal T <t.v.s10123@gmail.com>
2024-11-25 21:50:17 +0530

    add no arg constructor to Announcement()

--- src/main/kotlin/Announcement.kt
+++ src/main/kotlin/Announcement.kt
@@ -1,5 +1,5 @@
 data class Announcement(
-    var title: String,
+    var title: String = "Loading...",
     var message: String? = null,
     var imagePath: String? = null,
     var videoPath: String? = null,
