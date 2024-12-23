87417ad60a097678c25d59e38ef54e9a4114c063
Vishnu Sanal T <t.v.s10123@gmail.com>
2024-12-23 19:57:43 +0530

    chore: refactor

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,9 @@
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import com.google.gson.GsonBuilder
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -13,52 +16,51 @@ val firebaseDatabaseAPI: FirebaseDatabaseAPI =
         ).build()
     ).addConverterFactory(
         GsonConverterFactory.create(
-            GsonBuilder()
-                .registerTypeAdapter(Announcement::class.java, AnnouncementAdapter())
-                .setLenient()
-                .serializeNulls()
-                .create()
+            GsonBuilder().registerTypeAdapter(Announcement::class.java, AnnouncementAdapter())
+                .setLenient().serializeNulls().create()
         )
     ).build().create(FirebaseDatabaseAPI::class.java);
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
         App()
+    }
+}
 
-//        GlobalScope.launch {
-//            firebaseDatabaseAPI.setAnnouncements(
-//                listOf(
-//                    Announcement(
-//                        "College buses depart at 14:30 today",
-//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
-//                    ),
-//                    Announcement(
-//                        "Classes are suspended from 15:00",
-//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
-//                    ),
-//                    Announcement(
-//                        "Library will be open till 21:30 until exams are over",
-//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
-//                    ),
-//                    Announcement(
-//                        "Knimbus Digital Library registration ends tomorrow",
-//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
-//                    ),
-//                    Announcement(
-//                        "S2 exam registration deadline extended",
-//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
-//                    ),
-//                    Announcement(
-//                        "Digital Signage system back up again!",
-//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
-//                    ),
-//                    Announcement(
-//                        "Tomorrow is a holiday!",
-//                        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
-//                    ),
-//                )
-//            )
-//        }
-
+@OptIn(DelicateCoroutinesApi::class)
+private fun publishDummyData() {
+    GlobalScope.launch {
+        firebaseDatabaseAPI.setAnnouncements(
+            listOf(
+                Announcement(
+                    "College buses depart at 14:30 today",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+                ),
+                Announcement(
+                    "Classes are suspended from 15:00",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+                ),
+                Announcement(
+                    "Library will be open till 21:30 until exams are over",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+                ),
+                Announcement(
+                    "Knimbus Digital Library registration ends tomorrow",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+                ),
+                Announcement(
+                    "S2 exam registration deadline extended",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+                ),
+                Announcement(
+                    "Digital Signage system back up again!",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+                ),
+                Announcement(
+                    "Tomorrow is a holiday!",
+                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas blandit vitae augue sed gravida. In condimentum pharetra placerat. Nulla at metus efficitur, tristique enim et, cursus nulla."
+                ),
+            )
+        )
     }
 }
