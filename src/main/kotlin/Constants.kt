616a58a36184eba88d54a963673c5085d9ddc1e9
Vishnu Sanal T <t.v.s10123@gmail.com>
2025-02-05 12:08:54 +0530

    debug build

--- src/main/kotlin/Constants.kt
+++ src/main/kotlin/Constants.kt
@@ -10,4 +10,5 @@ object Constants {
     val LOGLEVEL: HttpLoggingInterceptor.Level = HttpLoggingInterceptor.Level.BASIC
 
     const val DB_BASE_URL = "https://digital-signage-gec-pkd-default-rtdb.firebaseio.com/"
+//    const val DB_BASE_URL = "https://gedigitalsignage-gecpkd-default-rtdb.firebaseio.com/" // DEBUG
 }
